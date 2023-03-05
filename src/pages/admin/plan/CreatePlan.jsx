44a865c25378aeb3c30307469e138aee2918789f
D'Arcy <walterluke1612@gmail.com>
2023-03-06 00:25:45 +0630

    import input form

--- src/pages/admin/plan/CreatePlan.jsx
+++ src/pages/admin/plan/CreatePlan.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
+import InputForm from "../../../components/form/InputForm";
 
 const CreatePlan = ({setShowCreateNewPlan}) => {
   return (
