6773d560de9d703b1056811473504e4f3e97a885
Solo Hsu <53265779+SoloHsu-coder@users.noreply.github.com>
2023-03-11 21:25:40 +0630

    login / isAdmin data binding

---
+++ src/service/api/UserAuth.js
@@ -0,0 +1,46 @@
+import { URL } from "./api_endpoint";
+
+export const userRegister = async (data) => {
+  const requestOption = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(`${URL}auths/signup`, requestOption);
+    const result = await response.json();
+
+    if (!response.ok) throw new Error(result.message);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const userLogin = async (data) => {
+  const requestOption = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(`${URL}auths/signin`, requestOption);
+    const result = await response.json();
+    if (!response.ok) throw new Error(result.message);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
