b931be38fc58d0e1da151be3818d1d768b31b114
D'Arcy <walterluke1612@gmail.com>
2023-03-23 18:56:17 +0630

    folder structure and libraries added

--- src/pages/admin/books/UpdataBook.jsx
+++ src/pages/admin/books/UpdataBook.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Chapter from './Chapter';
 
 const UpdataBook = () => {
     const {id} = useParams();
   return (
-    <div>UpdataBook {id}</div>
-  )
+		<>
+			<div>UpdataBook {id}</div>
+      <hr className='my-4' />
+      <Chapter />
+		</>
+	);
 }
 
-export default UpdataBook
+export default UpdataBook;
